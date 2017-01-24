6db26afc9a1b04c01d7b7d570c65569d838bfb84
ps <przemo509@gmail.com>
2017-01-24 04:56:55 +0100

    Niezainicjalizowane wskaźniki zawierały śmieci zamiast NULLi.

--- src/utils/LinkedListNode.h
+++ src/utils/LinkedListNode.h
@@ -1,9 +1,15 @@
 #ifndef MAGISTERKA_LINKEDLISTNODE_H
 #define MAGISTERKA_LINKEDLISTNODE_H
 
+#include <cstdlib>
+
 class LinkedListNode {
 public:
     LinkedListNode *prev, *next;
+
+    LinkedListNode() {
+        prev = next = NULL;
+    }
 };
 
 #endif //MAGISTERKA_LINKEDLISTNODE_H
