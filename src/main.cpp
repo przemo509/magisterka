7e1861f158ad7f61364e86037e5ef31cc9138116
ps <przemo509@gmail.com>
2017-01-25 21:09:31 +0100

    Wypisywanie na ekran również czasu trwania całego programu.

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,46 @@
 #include "utils/Timer.h"
 #include "utils/ExternalRenderer.h"
 
+void logFrameProcessingTime(int currentFrame, long frameStartTime, long programStartTime) {
+    // obliczenie szacowanego czasu zakończenia programu
+    float percent = 100.0f * currentFrame / Config::getInstance()->maxFrames;
+    long currentTime = Timer::getInstance().getCurrentTime();
+    long runningTime = currentTime - programStartTime;
+    long etaS = (int) (runningTime * 100.0f / percent - runningTime);
+    long etaH = etaS / 3600;
+    etaS -= etaH * 3600;
+    long etaM = etaS / 60;
+    etaS -= etaM * 60;
+    struct tm *estimatedEnd = localtime(&currentTime);
+    estimatedEnd->tm_sec += etaS;
+    estimatedEnd->tm_min += etaM + estimatedEnd->tm_sec / 60;
+    estimatedEnd->tm_hour += etaH + estimatedEnd->tm_min / 60;
+    estimatedEnd->tm_sec %= 60;
+    estimatedEnd->tm_min %= 60;
+    estimatedEnd->tm_hour %= 24;
+
+    Logger::getInstance()->info("Klatka %s z %d (%s%%) wygenerowana w %s s. Koniec za: %s:%s:%s, czyli o: %s:%s:%s",
+                                intToString(currentFrame, 3, ' ').c_str(),
+                                Config::getInstance()->maxFrames,
+                                intToString((int) percent, 3, ' ').c_str(),
+                                intToString(currentTime - frameStartTime, 3, ' ').c_str(),
+                                intToString(etaH, 2, '0').c_str(),
+                                intToString(etaM, 2, '0').c_str(),
+                                intToString(etaS, 2, '0').c_str(),
+                                intToString(estimatedEnd->tm_hour, 2, '0').c_str(),
+                                intToString(estimatedEnd->tm_min, 2, '0').c_str(),
+                                intToString(estimatedEnd->tm_sec, 2, '0').c_str()
+    );
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         cerr << "Błędne wywołanie, spróbuj tak:";
         cerr << "%s plik_konfiguracyjny" << argv[0];
         exit(EXIT_FAILURE);
     }
+
+    long programStartTime = Timer::getInstance().getCurrentTime();
 
     string configName = string(argv[1]);
     Config::init(configName);
@@ -20,7 +54,7 @@ int main(int argc, char **argv) {
     ExplosionSimulation *simulation = new ExplosionSimulation();
     ExternalRenderer *renderer = new ExternalRenderer(simulation);
     renderer->renderFrame(currentFrame);
-    Logger::getInstance()->info("Klatka %d wygenerowana w %d s", currentFrame, Timer::getInstance().getCurrentTime() - frameStartTime);
+    logFrameProcessingTime(currentFrame, frameStartTime, programStartTime);
 
     while (currentFrame < Config::getInstance()->maxFrames) {
         currentFrame = Timer::getInstance().incrementFrame();
@@ -32,12 +66,13 @@ int main(int argc, char **argv) {
 
         simulation->proceed();
         renderer->renderFrame(currentFrame);
-        Logger::getInstance()->info("Klatka %d wygenerowana w %d s", currentFrame, Timer::getInstance().getCurrentTime() - frameStartTime);
+        logFrameProcessingTime(currentFrame, frameStartTime, programStartTime);
     }
 
     renderer->makeVideo(currentFrame);
 
     Logger::getInstance()->info("Koniec przetwarzania %s", configName.c_str());
+    Logger::getInstance()->info("Czas trwania: %d s", Timer::getInstance().getCurrentTime() - programStartTime);
 
     delete renderer;
     delete simulation;
